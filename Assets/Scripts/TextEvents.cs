e2056ba1a3553c4e2e5980dde7bf85d3354f729b
Alex Wai Yiu Wong <aw987123@gmail.com>
2022-05-20 08:37:36 +0100

    Randomization complete every play. Only 1 text without the ability to refresh.

---
+++ Assets/Scripts/TextEvents.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextEvents : MonoBehaviour
+{
+    [SerializeField] private GameObject randomizedText;
+
+    private void Awake()
+    {
+
+    }
+}
---
+++ Assets/Scripts/TextRandomization.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextRandomization : MonoBehaviour
+{
+    [SerializeField] private List<string> duties = new List<string>();
+    //needs something the duties read from.
+
+    private TMP_Text tm_text;
+
+    private void OnEnable()
+    {
+        tm_text = this.gameObject.GetComponent<TMP_Text>();
+        tm_text.text = duties[Random.Range(0, duties.Count)];
+    }
+
+    public void NewDuty()
+    {
+        tm_text.text = duties[Random.Range(0, duties.Count)];
+    }
+
+    public void AddSingleDutyToList(string new_duty)
+    {
+        duties.Add(new_duty);
+    }
+
+    public void AddMultipleDutiesToList(List<string> new_duties)
+    {
+        foreach (var duty in new_duties)
+        {
+            duties.Add(duty);
+        }
+    }
+
+    public bool TryDeleteDutyFromString(string duty_to_delete)
+    {
+        return duties.Remove(duty_to_delete);
+    }
+
+    public bool TryDeleteDutyFromGameObject(GameObject obj_to_delete)
+    {
+        return obj_to_delete.GetComponent<TMP_Text>() != null ? duties.Remove(obj_to_delete.GetComponent<TMP_Text>().text) : false;
+    }
+}
